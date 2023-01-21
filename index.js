707a4623b15be13fdf960a22a28de376db99738e
Kwizera Theo <kwizerasinapy49@gmail.com>
2023-01-21 04:53:49 +0000

    some line of loops added

--- index.js
+++ index.js
@@ -14,9 +14,23 @@ while(a < 8){
 }
 console.log("------");
 // another one. loop counts from 2 to 9
-let c = 1;
+let c = 0;
 
-while(c < 9){
-  c ++;
+while(c < 10){
+  
   console.log(c);
+  c ++;
+}
+//in while loop, we can chane or move increament above or under console.log
+console.log("------");
+//added for loop. For loop is common in JavaScript. it's prefelable.
+
+for(let c = 0; c < 10; c ++){
+  console.log(c);
+}
+//in for loop, increament doesn't change.
+console.log("------");
+
+for(let f = 7; f <= 12; f ++){
+  console.log(f);
 }
